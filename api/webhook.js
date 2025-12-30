1b73ef725ab6b07684397d60b66d950a47d893f1
BimxyzDev <bimaputra436123@gmail.com>
2025-12-30 23:04:03 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -244,7 +244,7 @@ export default async function handler(req, res) {
   // GET untuk test
   if (req.method === 'GET') {
     return res.status(200).json({ 
-      bot: '@Cekidtelegrambimxyz_bot',
+      bot: '@@Cekidprobot',
       admins: ADMINS,
       status: 'active'
     })
